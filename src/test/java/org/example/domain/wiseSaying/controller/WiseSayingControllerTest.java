4c59e612cf0b5d57ece0f2d8ab326fd62cdf837a
sangxxjin <sj_0368@naver.com>
2024-11-26 14:22:24 +0900

    red 4

--- src/test/java/org/example/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/org/example/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
+import org.example.App;
 import org.example.standard.uttil.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,25 @@ public class WiseSayingControllerTest {
 
         assertThat(output.toString())
             .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        Scanner scanner = TestUtil.getScanner("""
+               목록
+               종료
+            """);
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output.toString())
+            .contains("명령) ");
     }
 }
