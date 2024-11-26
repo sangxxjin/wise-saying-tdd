62a1693975e7ada22bc7b2f38d72b0ac36503138
sangxxjin <sj_0368@naver.com>
2024-11-26 14:22:00 +0900

    green 3

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner scanner;
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public void run(){
+        System.out.println("== 명언 앱 ==");
+    }
+}
