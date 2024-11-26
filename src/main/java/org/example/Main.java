97616b12755e07c7337f67e1239a10cef09bdc56
sangxxjin <sj_0368@naver.com>
2024-11-26 12:38:00 +0900

    출력 데이터 연습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 //        lab1();
-        lab2();
+//        lab2();
+//        lab3();
+        lab4();
     }
 
     private static void lab1() {
@@ -27,6 +35,23 @@ public class Main {
         System.out.println("입력한 명령: " + cmd);
         cmd = scanner.nextLine().trim();
         System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab3() {
+        System.out.println(" == 시작 == ");
+        System.out.println("안녕하세요");
+        System.out.println("끝");
+    }
+
+    private static void lab4() throws IOException {
+        System.out.println(" == 시작 == ");
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        System.out.println("안녕하세요");
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        System.out.println("끝");
+        System.out.println("출력: " + output.toString());
+        output.close();
 
     }
 }
