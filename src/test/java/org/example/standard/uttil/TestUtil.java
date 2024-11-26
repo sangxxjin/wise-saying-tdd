886cf7b8541678c1e0cd2adba1d98aee2ab9912e
sangxxjin <sj_0368@naver.com>
2024-11-26 12:56:51 +0900

    green 1

---
+++ src/test/java/org/example/standard/uttil/TestUtil.java
@@ -0,0 +1,11 @@
+package org.example.standard.uttil;
+
+import java.util.Scanner;
+
+public class TestUtil {
+
+    public static Scanner getScanner(String input) {
+        return new Scanner(input);
+    }
+
+}
