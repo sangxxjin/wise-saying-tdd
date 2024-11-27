18c76305b4e888b39cdc7d880023c7341b948eeb
sangxxjin <sj_0368@naver.com>
2024-11-27 16:05:16 +0900

    green 36

--- src/main/java/org/example/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/org/example/domain/wiseSaying/entity/WiseSaying.java
@@ -5,15 +5,40 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class WiseSaying {
 
     private int id;
     private String content;
     private String author;
+
+    public WiseSaying(Map<String, Object> map) {
+        this.id = (int) map.get("id");
+        this.content = (String) map.get("content");
+        this.author = (String) map.get("author");
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        WiseSaying wiseSaying = (WiseSaying) other;
+        if (id != wiseSaying.id) {
+            return false;
+        }
+        if (!content.equals(wiseSaying.content)) {
+            return false;
+        }
+        return author.equals(wiseSaying.author);
+    }
 
     public boolean isNew() {
         return id == 0;
