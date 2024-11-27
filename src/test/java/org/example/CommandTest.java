6f1c77916b4c86fdb885d82d0273384227ecbbf5
sangxxjin <sj_0368@naver.com>
2024-11-27 10:10:59 +0900

    red 13

--- src/test/java/org/example/CommandTest.java
+++ src/test/java/org/example/CommandTest.java
@@ -5,10 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class CommandTest {
+
     @Test
     public void t1() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getActionName()).isEqualTo("삭제");
+    }
+
+    @Test
+    public void t2() {
+        Command cmd = new Command("삭제?id=10");
+        assertThat(cmd.getParam("id")).isEqualTo("10");
     }
 
 }
