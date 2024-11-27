5f09a14ab4e2299edac9fe3faa29130c99df3f84
sangxxjin <sj_0368@naver.com>
2024-11-27 10:10:11 +0900

    green 12

---
+++ src/main/java/org/example/Command.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Command {
+
+    private final String actionName;
+
+    public Command(String cmd) {
+        String[] cmtBits = cmd.trim().split("\\?");
+        this.actionName = cmtBits[0];
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+}
