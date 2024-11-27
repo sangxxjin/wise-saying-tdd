c220ad0405dde9628b67bf8c12244185d8cc3920
sangxxjin <sj_0368@naver.com>
2024-11-27 16:06:14 +0900

    blue 36

--- src/main/java/org/example/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/org/example/domain/wiseSaying/entity/WiseSaying.java
@@ -3,6 +3,7 @@ package org.example.domain.wiseSaying.entity;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class WiseSaying {
 
     private int id;
@@ -22,27 +24,11 @@ public class WiseSaying {
         this.content = (String) map.get("content");
         this.author = (String) map.get("author");
     }
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (other == null || getClass() != other.getClass()) {
-            return false;
-        }
-        WiseSaying wiseSaying = (WiseSaying) other;
-        if (id != wiseSaying.id) {
-            return false;
-        }
-        if (!content.equals(wiseSaying.content)) {
-            return false;
-        }
-        return author.equals(wiseSaying.author);
-    }
 
     public boolean isNew() {
         return id == 0;
     }
+
     public Map<String, Object> toMap() {
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("id", id);
