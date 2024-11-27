b524c5942395aae766d8c85ae2d4ffe318589ba7
sangxxjin <sj_0368@naver.com>
2024-11-27 14:40:52 +0900

    red / green 28

--- src/test/java/org/example/standard/uttil/JsonUtilTest.java
+++ src/test/java/org/example/standard/uttil/JsonUtilTest.java
@@ -40,7 +40,7 @@ public class JsonUtilTest {
     }
 
     @Test
-    @DisplayName("Map을 JSON으로 바꿀 수 있다.(숫자 필드)")
+    @DisplayName("Map을 JSON으로 바꿀 수 있다.(숫자(정수) 필드)")
     public void t3() {
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("id", 1);
@@ -54,6 +54,25 @@ public class JsonUtilTest {
                 "gender": "남자"
             }
             """.stripIndent().trim());
+    }
+
+    @Test
+    @DisplayName("Map을 JSON으로 바꿀 수 있다.(숫자(실수) 필드)")
+    public void t4() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("id", 1);
+        map.put("name", "이름");
+        map.put("gender", "남자");
+        map.put("height", 178.1543221);
+        String jsonStr = Util.json.toString(map);
+        assertThat(jsonStr).isEqualTo("""
+                {
+                    "id": 1,
+                    "name": "이름",
+                    "gender": "남자",
+                    "height": 178.1543221
+                }
+                """.stripIndent().trim());
     }
 
 }
