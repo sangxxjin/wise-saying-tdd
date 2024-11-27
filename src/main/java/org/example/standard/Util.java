f5bbd7987dc20e65ca73c69383d071807f5b0697
sangxxjin <sj_0368@naver.com>
2024-11-27 12:29:19 +0900

    green 24

--- src/main/java/org/example/standard/Util.java
+++ src/main/java/org/example/standard/Util.java
@@ -1,10 +1,14 @@
 package org.example.standard;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Util {
 
@@ -25,7 +29,22 @@ public class Util {
                 Files.writeString(path, content, StandardOpenOption.CREATE,
                     StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                final Path parentDir = path.getParent();
+                if (parentDir != null && Files.notExists(parentDir)) {
+                    try {
+                        Files.createDirectories(parentDir);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    try {
+                        Files.writeString(path, content, StandardOpenOption.CREATE,
+                            StandardOpenOption.TRUNCATE_EXISTING);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                } else {
+                    throw new RuntimeException(e);
+                }
             }
         }
 
@@ -38,17 +57,48 @@ public class Util {
             }
         }
 
-        public static void delete(String filePath) {
-            Path path = Paths.get(filePath);
+        public static boolean delete(String filePath) {
+            final Path path = Paths.get(filePath);
+
             try {
-                Files.delete(path);
+                Files.walkFileTree(path, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                        Files.delete(file); // 파일 삭제
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+                        throws IOException {
+                        Files.delete(dir); // 디렉토리 삭제
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                return true;
+            } catch (NoSuchFileException e) {
+                return false;
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                return false;
             }
         }
 
         public static boolean notExists(String filePath) {
             return !exists(filePath);
+        }
+
+        public static void mkdir(String dirPath) {
+            Path path = Paths.get(dirPath);
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static void rmdir(String filePath) {
+            delete(filePath);
         }
     }
 
