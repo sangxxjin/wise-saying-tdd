f9d0e1577dbede1bde460af5d672801b7925f5d5
sangxxjin <sj_0368@naver.com>
2024-11-27 09:27:36 +0900

    blue 9

--- src/main/java/org/example/domain/wiseSaying/entity/WiseSayingService.java
+++ src/main/java/org/example/domain/wiseSaying/entity/WiseSayingService.java
@@ -1,20 +1,23 @@
 package org.example.domain.wiseSaying.entity;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.example.domain.wiseSaying.repository.WiseSayingRepository;
+
 public class WiseSayingService {
-    private final List<WiseSaying> wiseSayings;
-    private int lastId;
+
+    private final WiseSayingRepository wiseSayingRepository;
+
     public WiseSayingService() {
-        this.wiseSayings = new ArrayList<>();
-        this.lastId = 0;
+        this.wiseSayingRepository = new WiseSayingRepository();
     }
+
     public WiseSaying add(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayings.add(wiseSaying);
+        WiseSaying wiseSaying = new WiseSaying(0, content, author);
+        wiseSayingRepository.save(wiseSaying);
         return wiseSaying;
     }
+
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
     }
 }
---
+++ src/main/java/org/example/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -0,0 +1,26 @@
+package org.example.domain.wiseSaying.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.example.domain.wiseSaying.entity.WiseSaying;
+
+public class WiseSayingRepository {
+
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingRepository() {
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        wiseSaying.setId(++lastId);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+}
