218fa6a16626fdcfee3957fd004a2b4831c0369b
sangxxjin <sj_0368@naver.com>
2024-11-27 15:28:36 +0900

    feat repository - interface, memory, file

---
+++ src/main/java/org/example/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -0,0 +1,42 @@
+package org.example.domain.wiseSaying.repository;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.example.domain.wiseSaying.entity.WiseSaying;
+
+public class WiseSayingFileRepository implements WiseSayingRepository {
+
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingFileRepository() {
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        if (!wiseSaying.isNew()) {
+            return wiseSaying;
+        }
+
+        wiseSaying.setId(++lastId);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+
+    public boolean deleteById(int id) {
+        return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
+    }
+
+    public Optional<WiseSaying> findById(int id) {
+        return wiseSayings.stream()
+            .filter(wiseSaying -> wiseSaying.getId() == id)
+            .findFirst();
+    }
+}
