35240237a832b90c126050ad2db3a8876b95f79a
sangxxjin <sj_0368@naver.com>
2024-11-27 16:05:10 +0900

    red 36

--- src/test/java/org/example/domain/wiseSaying/repository/wiseSayingFileRepositoryTest.java
+++ src/test/java/org/example/domain/wiseSaying/repository/wiseSayingFileRepositoryTest.java
@@ -2,6 +2,7 @@ package org.example.domain.wiseSaying.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
 import org.example.domain.wiseSaying.entity.WiseSaying;
 import org.example.standard.Util;
 import org.junit.jupiter.api.AfterAll;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class wiseSayingRepositoryTest {
+public class wiseSayingFileRepositoryTest {
 
     private final WiseSayingRepository wiseSayingRepository = new WiseSayingFileRepository();
 
@@ -27,10 +28,15 @@ public class wiseSayingRepositoryTest {
     @Test
     @DisplayName("명언 저장")
     public void t1() {
-        wiseSayingRepository.save(new WiseSaying(0, "명언1", "저자1"));
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
         String filePath = "db/test/wiseSaying/1.json";
         assertThat(
             Util.file.exists(filePath)
         ).isTrue();
+        String jsonStr = Util.file.get(filePath, "");
+        Map<String, Object> wiseSayingMap = Util.json.toMap(jsonStr);
+        WiseSaying wiseSayingRestored = new WiseSaying(wiseSayingMap);
+        assertThat(wiseSayingRestored).isEqualTo(wiseSaying);
     }
 }
