915033daebdaf3ac4150f803d48c7d97497460fd
sangxxjin <sj_0368@naver.com>
2024-11-27 10:40:05 +0900

    red/green 17

--- src/test/java/org/example/CommandTest.java
+++ src/test/java/org/example/CommandTest.java
@@ -59,4 +59,15 @@ public class CommandTest {
         assertThat(cmd.getParamAsInt("number", 0)).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("""
+            cmd.getParamAsInt("number", 0) == 10
+             and cmd.getName("name") == "Paul"
+            """)
+    public void t7() {
+        Command cmd = new Command("목록?number=10&name=Paul");
+        assertThat(cmd.getParamAsInt("number", 0)).isEqualTo(10);
+        assertThat(cmd.getParam("name")).isEqualTo("Paul");
+    }
+
 }
